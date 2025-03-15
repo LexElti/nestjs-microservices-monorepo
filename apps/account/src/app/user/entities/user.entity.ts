582a18072c8e22539801274c725fa56fbef114a3
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-15 21:16:42 +0300

    Модель User

---
+++ apps/account/src/app/user/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { genSalt, hash } from 'bcryptjs';
+import { IUser, UserRole } from '@nestjs-microservices-monorepo/interfaces';
+
+export class UserEntity implements IUser {
+  _id?: string;
+  displayName?: string;
+  email: string;
+  passwordHash: string;
+  role: UserRole;
+
+  constructor(user: IUser) {
+    this._id = user._id;
+    this.passwordHash = user.passwordHash;
+    this.displayName = user.displayName;
+    this.email = user.email;
+    this.role = user.role;
+  }
+
+  public async setPassword(password: string) {
+    const salt = await genSalt(10);
+    this.passwordHash = await hash(password, salt);
+    return this;
+  }
+}
--- apps/account/src/app/user/user.module.ts
+++ apps/account/src/app/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
+import { User, UserSchema } from './models/user.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UserService],
 })
 export class UserModule {}
