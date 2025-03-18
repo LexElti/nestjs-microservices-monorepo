156d08142dbe8b8d7a08cbdcd13f5d7103be609f
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-18 22:48:44 +0300

    Добавление библиотеки контрактов

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,4 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import {
+  AccountLogin,
+  AccountRegister,
+} from '@nestjs-microservices-monorepo/contracts';
 import { AuthService } from './auth.service';
 
 @Controller()
@@ -6,24 +10,17 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() dto: RegisterDto) {
+  async register(
+    @Body() dto: AccountRegister.Request
+  ): Promise<AccountRegister.Response> {
     return this.authService.register(dto);
   }
 
   @Post('login')
-  async login(@Body() { email, password }: LoginDto) {
+  async login(
+    @Body() { email, password }: AccountLogin.Request
+  ): Promise<AccountLogin.Response> {
     const { id } = await this.authService.validateUser(email, password);
     return this.authService.login(id);
   }
-}
-
-export class RegisterDto {
-  email: string;
-  password: string;
-  displayName?: string;
-}
-
-export class LoginDto {
-  email: string;
-  password: string;
 }
