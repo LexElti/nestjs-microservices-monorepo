e9d805cb88713b7c96185ec7fc1b731d00f9e6af
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-19 00:10:04 +0300

    Валидация запросов

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller } from '@nestjs/common';
-import { RMQRoute } from 'nestjs-rmq';
+import { RMQRoute, RMQValidate } from 'nestjs-rmq';
 import {
   AccountLogin,
   AccountRegister,
@@ -10,6 +10,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @RMQValidate()
   @RMQRoute(AccountRegister.topic)
   async register(
     @Body() dto: AccountRegister.Request
@@ -17,6 +18,7 @@ export class AuthController {
     return this.authService.register(dto);
   }
 
+  @RMQValidate()
   @RMQRoute(AccountLogin.topic)
   async login(
     @Body() { email, password }: AccountLogin.Request
