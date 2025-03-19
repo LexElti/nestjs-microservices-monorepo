fc48089db3aa324fe5a4b82daa33c2b3406ea5fd
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-19 12:55:00 +0300

    Авторизация на API

---
+++ apps/api/src/app/strategies/jwt.straragy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IJWTPayload } from '@nestjs-microservices-monorepo/interfaces';
+
+@Injectable()
+export class JwtStratagy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate({ id }: IJWTPayload) {
+    return id;
+  }
+}
