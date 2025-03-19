7e26becc836ad2ef4d775db569bb042c7ce6a6aa
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2025-03-20 00:11:31 +0300

    Реализация User Queries

--- apps/account/src/app/user/user.module.ts
+++ apps/account/src/app/user/user.module.ts
@@ -3,12 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { User, UserSchema } from './models/user.model';
 import { UserRepository } from './repositories/user.repository';
+import { UserCommands } from './user.commands';
+import { UserQueries } from './user.queries';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UserRepository, UserService],
+  controllers: [UserCommands, UserQueries],
   exports: [UserRepository],
 })
 export class UserModule {}
---
+++ apps/account/src/app/user/user.queries.ts
@@ -0,0 +1,34 @@
+import { Body, Controller } from '@nestjs/common';
+import {
+  AccountUserCourses,
+  AccountUserInfo,
+} from '@nestjs-microservices-monorepo/contracts';
+import { RMQValidate, RMQRoute } from 'nestjs-rmq';
+import { UserRepository } from './repositories/user.repository';
+
+@Controller()
+export class UserQueries {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  @RMQValidate()
+  @RMQRoute(AccountUserInfo.topic)
+  async userInfo(
+    @Body() { id }: AccountUserInfo.Request
+  ): Promise<AccountUserInfo.Response> {
+    const user = await this.userRepository.findUserById(id);
+    return {
+      user,
+    };
+  }
+
+  @RMQValidate()
+  @RMQRoute(AccountUserCourses.topic)
+  async userCourses(
+    @Body() { id }: AccountUserCourses.Request
+  ): Promise<AccountUserCourses.Response> {
+    const user = await this.userRepository.findUserById(id);
+    return {
+      courses: user.courses,
+    };
+  }
+}
